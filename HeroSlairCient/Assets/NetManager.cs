83e59f783709136c7815588e855a74796d103f65
Josh6714 <jalbo@uccs.edu>
2015-10-15 13:50:08 -0600

    Project
    JUST DO IT!!!!

---
+++ HeroSlairCient/Assets/NetManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetManager : NetworkManager {
+
+	int connectionId = -1;
+	const short clientMsgType = 1002;
+	const short serverMsgType = 1003;
+
+    public void Start()
+    {
+        SetUpClient();
+        NetworkServer.RegisterHandler(serverMsgType, OnClientReadyToBeginMessage);
+        NetworkServer.RegisterHandler(clientMsgType, OnClientReadyToBeginMessage);
+    }
+
+    public void SendReadyToBeginMessage(int myId)
+    {
+        if (connectionId != -1)
+        {
+            Debug.Log("Attempting to send to " + connectionId);
+            var msg = new LevelMessage();
+            msg.width = 10;
+            msg.height = 20;
+            NetworkServer.SendToClient(connectionId, serverMsgType, msg);
+        }
+        else
+        {
+            Debug.Log("ERROR: Not connected to client");
+        }
+    }
+
+    void OnClientReadyToBeginMessage(NetworkMessage netMsg)
+    {
+        var beginMessage = netMsg.ReadMessage<LevelMessage>();
+        Debug.Log("received OnClientReadyToBeginMessage " + beginMessage.message);
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        base.OnServerConnect(conn);
+        connectionId = conn.connectionId;
+        Debug.Log("Client Connected: " + connectionId);
+    }
+
+    //client connects to server
+    public void SetUpClient()
+    {
+        //Network.Connect("localhost", 4444);
+        StartClient();
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        Debug.Log("Actually Connected blah");
+
+    }
+    //when connected to server
+    void OnConnectedToServer()
+    {
+        Debug.Log("Actually Connected");
+    }
+}
---
+++ HeroSlairCient/Assets/playerMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed;
+    public bool exit = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, speed, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -speed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "DoorExit")
+        {
+            //exit = true;
+            GameObject.FindObjectOfType<MessageSender>().MsgType = 1012;
+            GameObject.FindObjectOfType<MessageSender>().SendReadyToBeginMessage(0);
+        }
+    }
+}
